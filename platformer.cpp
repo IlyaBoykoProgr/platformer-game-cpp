a7e8fb67ed15ff350e575b9ebbe25e6705cf22da
IlyaBoykoProgr <44905605+IlyaBoykoProgr@users.noreply.github.com>
2018-11-18 14:15:42 +0300

    not stability now!
    please don't downoload, i'm programming...

--- platformer.cpp
+++ platformer.cpp
@@ -60,15 +60,15 @@
 #endif
 using namespace std;
 
-char table[4][10]={
-{' ',' ',' ',' ',' ',' ',' ',' ',' '},
-{' ',' ',' ',' ',' ',' ',' ',' ',' '},
-{' ',' ',' ',' ',' ',' ',' ',' ',' '},
-{' ',' ',' ',' ',' ',' ',' ',' ',' '}
+char table[5][10]={
+{' ',' ',' ',' ',' ',' ',' ',' ',' ',' '},
+{' ',' ',' ',' ',' ',' ',' ',' ',' ',' '},
+{' ',' ',' ',' ',' ',' ',' ',' ',' ',' '},
+{' ',' ',' ',' ',' ',' ',' ',' ',' ',' '},
+{' ',' ',' ',' ',' ',' ',' ',' ',' ',' '}
 };
-char person='&';
-char block='%';
-int pos;//позиция персонажа
+char person='L';
+char block='Z';
 char Play[7]= "Play";//в меню когда не играл еще
 
 // \x1b[ <аттрибуты вывода> m
@@ -76,54 +76,27 @@ char Play[7]= "Play";//в меню когда не играл еще
 
 
 void logic(){
-table[0][0]=table[0][1];
-table[0][1]=table[0][2];
-table[0][2]=table[0][3];
-table[0][3]=table[0][4];
-table[0][4]=table[0][5];//уничтожение не нужного
-table[0][5]=table[0][6];
-table[0][6]=table[0][7];
-table[0][7]=table[0][8];
-table[0][8]=table[0][9];
-table[0][9]=' ';
-table[1][0]=table[1][1];
-table[1][1]=table[1][2];
-table[1][2]=table[1][3];
-table[1][3]=table[1][4];
-table[1][4]=table[1][5];
-table[1][5]=table[1][6];//Перерисовка карты
-table[1][6]=table[1][7];//справа налево.
-table[1][7]=table[1][8];
-table[1][8]=table[1][9];
-table[1][9]=' ';
-table[2][0]=table[2][1];
-table[2][1]=table[2][2];
-table[2][2]=table[2][3];
-table[2][3]=table[2][4];
-table[2][4]=table[2][5];
-table[2][5]=table[2][6];//Перерисовка карты
-table[2][6]=table[2][7];//справа налево.
-table[2][7]=table[2][8];
-table[2][8]=table[2][9];
-table[2][9]=' ';
-table[3][0]=table[3][1];
-table[3][1]=table[3][2];
-table[3][2]=table[3][3];
-table[3][3]=table[3][4];
-table[3][4]=table[3][5];
-table[3][5]=table[3][6];//Перерисовка карты
-table[3][6]=table[3][7];//справа налево.
-table[3][7]=table[3][8];
-table[3][8]=table[3][9];
-table[3][9]=' ';
+for(int i=0; i<6; i++){
+table[i][0]=table[i][1];
+table[i][1]=table[i][2];
+table[i][2]=table[i][3];
+table[i][3]=table[i][4];
+table[i][4]=table[i][5];//уничтожение не нужного
+table[i][5]=table[i][6];
+table[i][6]=table[i][7];
+table[i][7]=table[i][8];
+table[i][8]=table[i][9];
+table[i][9]=' ';}
 }//МНОГО
 
 void showtable(){//показ текущей карты
+CLEAR
 cout<<"___________\n";//потолок
 cout<<table[0][0]<<table[0][1]<<table[0][2]<<table[0][3]<<table[0][4]<<table[0][5]<<table[0][6]<<table[0][7]<<table[0][8]<<table[0][9]<<"\n";
 cout<<table[1][0]<<table[1][1]<<table[1][2]<<table[1][3]<<table[1][4]<<table[1][5]<<table[1][6]<<table[1][7]<<table[1][8]<<table[1][9]<<"\n";
 cout<<table[2][0]<<table[2][1]<<table[2][2]<<table[2][3]<<table[2][4]<<table[2][5]<<table[2][6]<<table[2][7]<<table[2][8]<<table[2][9]<<"\n";
 cout<<table[3][0]<<table[3][1]<<table[3][2]<<table[3][3]<<table[3][4]<<table[3][5]<<table[3][6]<<table[3][7]<<table[3][8]<<table[3][9]<<"\n";
+cout<<table[4][0]<<table[4][1]<<table[4][2]<<table[4][3]<<table[4][4]<<table[4][5]<<table[4][6]<<table[4][7]<<table[4][8]<<table[4][9]<<"\n";
 cout<<"==========\n";//это 'лава'
 }//по другому мой комп не считывает.Прямо надо.
 
@@ -187,83 +160,68 @@ CLEAR
 cout<<"ERROR";
 WAIT
 return;
- }
- //                                   настройка персонажа
+ }//                                   настройка персонажа
 }else if(t=='3'){
 cout<<"OPTIONS:\n your persone:\n(enter symbol)\n";
-cin>>person;
+person=getchar();
 WAIT
 }else if(t=='4'){
 cout<<"OPTIONS:\n blocks:\n(enter symbol)\n";
-cin>>block;
-WAIT//                                  настройка формы блоков
+block=getchar();
+WAIT //                                  настройка формы блоков
 }else{
 options();
 return;}
 }
 
-int play(){//                        ИГРА
-cout<<"3\n";        //|
-cout<<"2\n";        //|красивое
-cout<<"1\n";        //|начало       
-cout<<"start!!!\n"; //|
-WAIT                //|
-CLEAR               //|
+void play(){//                     '''''''   ИГРА   ''''''''  
+cout<<"start!!!\n";
+WAIT
+CLEAR
 
-table[3][0]=block;table[3][1]=block;table[3][2]=block;table[3][3]=block; //
-table[3][4]=block;table[3][5]=block;table[3][6]=block;table[3][7]=block; //начало карты
-table[3][8]=block;table[3][9]=block;                                     //
+table[4][0]=block;table[4][1]=block;table[4][2]=block;table[4][3]=block; //
+table[4][4]=block;table[4][5]=block;table[4][6]=block;table[4][7]=block; //начало карты
+table[4][8]=block;table[4][9]=block;                                     //
 
-bool lose=false;//в ином случае проиграешь
-pos=0;//начальная позиция
-int score=0;
+int pos=0;//начальная позиция
+int score=0;//наш будущий счет
 
-while(!lose){
-
+while(1){
 table[pos][0]=person;//появление персонажа
 
-WAIT                  //}
-CLEAR                 //}основная логика
-showtable();          //}
-logic();              //}
-if(table[pos][1]==block){lose=true;}//врезался-умер
+WAIT //чуть-чуть ждем
+showtable();//показ карты
 
-int flat=rand()%3+1; //|где блок ставить?
-table[flat][9]=block;//|а теперь ставим.
+int flat=rand()%4+1; //где блок ставить?
+table[flat][9]=block;//а теперь ставим.
 
-if(table[pos+1][0]==' '){pos++;}//падаем
-else if(kbhit()){pos--;score++;}//прыг!(только не по воздуху).пусть за прыжок дополнительные очки
-score++;//здесь мы прошли блок,счет растет
+if(table[pos][1]==block)break;//врезался-умер
+if(pos==3)break;//падаем-умираем
+score++;//здесь мы прошли блок,счет растет.Можно играть дальше
 
-if(pos==3){lose=true;}//падаем-умираем
+if(table[pos+1][0]==' ')pos++;//падаем
+else if(kbhit()){pos--;score++;}//прыг!.пусть за прыжок дополнительные очки
+logic(); //перерисовка
 }
-CLEAR
-showtable();
-#ifdef linux
-cout<<"\nGAME OVER!\npress 'q' to exit.";//когда все кончено...
+cout<<"\nGAME OVER!\npress 'q' to exit.\n";//когда все кончено...
 while(!(getch()=='q'));
-#elif _WIN32
-MessageBox(NULL,"you lose!","Message",MB_OK|MB_ICONSTOP);
-#endif
-Play[0]='R';Play[1]='e';Play[2]='s';Play[3]='p';Play[4]='a';Play[5]='w';Play[6]='n';//это пишем в меню когда проиграл вместо плей
-return score;//cчет возвращаем
+Play[0]='R';Play[1]='e';Play[2]='s';Play[3]='p';Play[4]='a';Play[5]='w';Play[6]='n';
+cout<<"Your score is "<<score<<'\n';//cчет возвращаем
+WAIT
 }
 
 int main(){
 CLEAR	
-cout<<"PLATFORMER:\n\nMENU:\n1."<<Play<<"\n2.Options\n3.Help\n";
+cout<<"PLATFORMER:\n\nMENU:\n1."<<Play<<"\n2.Options\n3.Help\n4.Exit...\n";
 char num=getch();
-if(num=='1'){
-   CLEAR 
-   int a=play();
-   cout<<"Your score: "<<a<<"\n";
-   WAIT
+CLEAR
+if(num=='1'){ 
+   play();
 }else if(num=='2'){
-   CLEAR 
    options();
 }else if(num=='3'){
-   CLEAR 
-   cout<<"HELP:\nThis game is 'platformer'.You need jump trough the abysses & don't birn into a block.Space to jump.\n";
-   getch(); }
+   cout<<"HELP:\nThis game is 'platformer'.You need jump trough the abysses & don't birn into a block.Space to jump.\n\nA fact:'Escape'to exit!\nP.S. Programm made by ilya https://github.com/IlyaBoykoProgr";
+   getch(); 
+}else if(num=='4'||num=='\E')return 0;
 main();
 }
