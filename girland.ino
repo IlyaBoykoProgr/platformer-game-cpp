652a8172e06c3f634f9ef2a7e59fb7cef32df349
IlyaBoykoProgr <44905605+IlyaBoykoProgr@users.noreply.github.com>
2019-08-21 11:27:14 +0300

    3d and 4th mode

--- girland.ino
+++ girland.ino
@@ -1,9 +1,11 @@
 #include "Adafruit_NeoPixel.h"
-#define PIXELS 60
-#define PIN 8
-#define BRIGHTNESS 100
-#define MODE 2
-
+#define PIXELS 60 // count of pixels in the lent | кол-во пикселей в ленте
+#define PIN 8 //pin with the adress lent | пин с адресной лентой
+#define BRIGHTNESS 100 // you understood | яркость
+#define MODE 4 // the mode | режим
+/*  The random mode is | случайный режим задается 
+    random(0,5)
+*/
 Adafruit_NeoPixel pix(PIXELS,PIN);
 
 void fillRange(byte from, byte to, uint32_t color=pix.Color(0,0,0)){
@@ -22,14 +24,14 @@ void setup(){
 void loop(){
    uint32_t color = pix.Color(random(0,255),random(0,255),random(0,255));
    switch (MODE){
-     case 0:
+     case 0://snake | змейка
        for(byte i=0; i<PIXELS; i++){
          pix.setPixelColor(i,color);
          pix.show();
          delay(10);
        }
      break;
-     case 1:{
+     case 1:{//double snake | двойная змейка
        byte i=random(0,2);
        for(i; i<PIXELS; i+=2){
          pix.setPixelColor(i,color);
@@ -38,7 +40,7 @@ void loop(){
        delay(600);
      }
      break;
-     case 2:
+     case 2://moving line | бегающая линия
        for(byte j=0; j<PIXELS-4; j++){
          fillRange(j,j+5,color);
          delay(3);
@@ -47,10 +49,36 @@ void loop(){
        }
        for(byte j=PIXELS-5; j>0; j--){
          fillRange(j,j+5,color);
-         delay(3);
+         delay(5);
          fillRange(j,j+5);
-         delay(3);
        }
+     break;
+     case 3://pushers) | пихалки
+       for(byte i=0; i<PIXELS; i++){
+         for(byte j=0; j<i; j++){
+           pix.setPixelColor(j,color);
+           pix.show();
+           delay(5);
+           pix.setPixelColor(j,0,0,0);
+           pix.show();
+         }
+         pix.setPixelColor(i,0,0,255);
+       }
+       pix.clear();
+     break;
+     case 4://beads | бусины
+       for(byte i=PIXELS-1; i>0; i--){
+         for(byte j=0; j<i; j++){
+           pix.setPixelColor(j,color);
+           pix.show();
+           delay(5);
+           pix.setPixelColor(j,0,0,0);
+           pix.show();
+         }
+         pix.setPixelColor(i,color);
+         color = pix.Color(random(0,255),random(0,255),random(0,255));
+       }
+       pix.clear();
      break;
    }
 }
