918671886e5397e0a21c50ffddbed0bc5a6fa686
IlyaBoykoProgr <44905605+IlyaBoykoProgr@users.noreply.github.com>
2020-07-14 12:44:54 +0300

    Add files via upload

--- maze.cpp
+++ maze.cpp
@@ -1,36 +1,38 @@
 /*
- programm made by IllyaBoyko https://github.com/IllyaBoykoProgr
+ programm made by IlyaBoyko https://github.com/IlyaBoykoProgr
  start programm by terminal: 'maze play' to play or 'maze help' to help
 */
 #include<iostream>
+#include<time.h>
 #include<cstdlib>
 #include<stdio.h>
 #include<stdlib.h>
 #include<fstream>
-#ifdef WINDOWS
- #include<windows.h>
- #include<conio.h>
- #define WAIT(h) Sleep(h)
- #define CLEAR for(int i;i<100;i++)cout<<endl
+#ifdef _unix_
+    #include<ctime>
+    #include<termios.h>
+    #include<unistd.h>
+    #define WAIT(h) usleep(h)
+    #define CLEAR system("clear");
+    char getch(){
+        struct termios oldt,newt;
+        int ch;
+        tcgetattr( STDIN_FILENO, &oldt );
+        newt = oldt;
+        newt.c_lflag &= ~( ICANON | ECHO );
+        tcsetattr( STDIN_FILENO, TCSANOW, &newt );
+        ch = getchar();
+        tcsetattr( STDIN_FILENO, TCSANOW, &oldt );
+    return ch;
+    }
 #else
- #include<ctime>
- #include<termios.h>
- #include<unistd.h>
- #define WAIT(h) usleep(h)
- #define CLEAR system("clear");
- char getch(){
- struct termios oldt,newt;
- int ch;
- tcgetattr( STDIN_FILENO, &oldt );
- newt = oldt;
- newt.c_lflag &= ~( ICANON | ECHO );
- tcsetattr( STDIN_FILENO, TCSANOW, &newt );
- ch = getchar();
- tcsetattr( STDIN_FILENO, TCSANOW, &oldt );
- return ch;}
+    #include<windows.h>
+    #include<conio.h>
+    #define WAIT(h) Sleep(h)
+    #define CLEAR system("cls")//for(int i=0;i<20;i++)cout<<endl
 #endif
-#define HEIGHT rand()%40//любое кол-во строк      |any count of strings(vertical)
-#define WIDTH rand()%40 //любое кол-во длины строки|any count of column(horisontal)
+#define HEIGHT rand()%20//любое кол-во строк       |any count of strings(vertical)
+#define WIDTH rand()%100 //любое кол-во длины строки|any count of column(horisontal)
 using namespace std;
 bool deadend(int, int, int**, int, int); // Вспомогательная функция, определяет тупики
 void visual(int**, int, int, int); // Изображение результата с помощью консольной графики
@@ -67,6 +69,10 @@ person Iam,second;
 
 int main(int argc, char *argv[]){
 bool walkthroughwalls=0;
+time_t* t=new time_t;
+time(t);
+srand(*t);
+delete t;
 twoplayers=0;
 int height = HEIGHT, width = WIDTH;
 if(height%2==0)height++;
@@ -79,7 +85,7 @@ if(argc>1)switch(*argv[1]){
   cout<<"maze version- version\nmaze play- play\nmaze cheat- cheats(walk through walls)\nmaze invisible- invisible walls\nmaze two- two players\n\nThanks for using terminal!\n";
   return 0;
  case 'v':
-  cout<<"Version: 2.6\nBugs kill...\n";
+  cout<<"Version: 3.0\nWindows update\n";
   return 0;
  case 'c':
   walkthroughwalls=1;
@@ -201,35 +207,35 @@ void help(int height, int width){
 }
 
 bool deadend(int x, int y, int** maze, int height, int width){
-	int a = 0;
+    int a = 0;
 
-	if(x != 1){
-		if(maze[y][x-2] == 1)
-			a+=1;
-		}
-	else a+=1;
+    if(x != 1){
+        if(maze[y][x-2] == 1)
+            a+=1;
+        }
+    else a+=1;
 
-	if(y != 1){
-		if(maze[y-2][x] == 1)
-			a+=1;
-		}
-	else a+=1;
+    if(y != 1){
+        if(maze[y-2][x] == 1)
+            a+=1;
+        }
+    else a+=1;
 
-	if(x != width-2){
-		if(maze[y][x+2] == 1)
-			a+=1;
-		}
-	else a+=1;
+    if(x != width-2){
+        if(maze[y][x+2] == 1)
+            a+=1;
+        }
+    else a+=1;
 
-	if(y != height-2){
-		if(maze[y+2][x] == 1)
-			a+=1;
-		}
-	else a+=1;
+    if(y != height-2){
+        if(maze[y+2][x] == 1)
+            a+=1;
+        }
+    else a+=1;
 
-	if(a == 4)
-		return 1;
-	return 0;
+    if(a == 4)
+        return 1;
+    return 0;
 }
 
 void win(int** maze, int height,int width){
@@ -240,112 +246,122 @@ void win(int** maze, int height,int width){
   if(maze[x][y]==0){i--;continue;}
   maze[x][y]=0;
   visual(maze,height,width,rand()%4);
+  #ifdef _unix_
   WAIT(100);
+  #endif
  }
 }
 
 void visual(int** maze, int height, int width,int c){
  CLEAR;
-	for(int i = 0; i < height; i++){
-		for(int j = 0; j < width; j++){
-		    if(i==height-2&&j==width-2){
-		    #ifdef WINDOWS
-		    cout<<"O";
-		    #else
-		    cout<<"\E[41m \E[0m";
-		    #endif
-		    continue;}
-		    if(Iam.getx()==i && Iam.gety()==j){
-		    #ifdef WINDOWS
-		    cout<<"1";
-		    #else
-		    cout<<"\E[42m \E[0m";
-		    #endif
-		    continue;}
-		    if(twoplayers && second.getx()==i && second.gety()==j){
-		    #ifdef WINDOWS
-		    cout<<"2";
-		    #else
-		    cout<<"\E[42m \E[0m";
-		    #endif
-		    continue;}
-			switch(maze[i][j]){
-				case 0:
-				#ifdef WINDOWS
-				if(c<4)cout<<"%";
-				else cout<<" ";
-				break;
-				#else
-				switch(c){
-				 case 0:
-				 cout<<"\E[43;33m";break;
-				 case 1:
-				 cout<<"\E[44;34m";break;
-				 case 2:
-				 cout<<"\E[45;35m";break;
-				 case 3:
-				 cout<<"\E[46;36m";break;
-				 case 4:
-				 cout<<"\E[40;30m";break;//invisible
-				}
-				cout<<"0\E[0m"; break;
-				 #endif
-				case 1: cout<<" "; break;
-				case 2: cout<<"\E[47m*\E[0m";break;
-			}
+    for(int i = 0; i < height; i++){
+        for(int j = 0; j < width; j++){
+            if(i==height-2&&j==width-2){
+                #ifdef _unix_
+                    cout<<"\E[41m \E[0m"
+                #else
+                    cout<<"O";
+                #endif
+                continue;
+            }
+            if(Iam.getx()==i && Iam.gety()==j){
+                #ifdef _unix_
+                    cout<<"\E[42m \E[0m";
+                #else
+                    cout<<"1";
+                #endif
+                continue;
+            }
+            if(twoplayers && second.getx()==i && second.gety()==j){
+                #ifdef _unix_
+                    cout<<"\E[42m \E[0m";
+                #else
+                    cout<<"2";
+                #endif
+                continue;
+            }
+            switch(maze[i][j]){
+                case 0:
+                #ifdef _unix_
+                switch(c){
+                 case 0:
+                 cout<<"\E[43;33m";break;
+                 case 1:
+                 cout<<"\E[44;34m";break;
+                 case 2:
+                 cout<<"\E[45;35m";break;
+                 case 3:
+                 cout<<"\E[46;36m";break;
+                 case 4:
+                 cout<<"\E[40;30m";break;//invisible
+                }
+                cout<<"0\E[0m"; break;
+                #else
+                cout<<((c<4)?"%":" ");
+                break;
+                #endif
+                case 1: cout<<" "; break;
+                #ifdef _unix_
+                    case 2:cout<<"\E[47m*\E[0m";break;
+                #else
+                    case 2:cout<<"#";break;
+                #endif
+            }
         }
-		cout<<endl;
+        cout<<endl;
     }
    // cout<<"x:"<<Iam.getx()<<" y:"<<Iam.gety()<<'\n';
 }
 
 void mazemake(int** maze, int height, int width){
-	int x, y, c, a;
+    int x, y, c, a;
+    for(int i = 0; i < height; i++) // Массив заполняется землей-ноликами
+        for(int j = 0; j < width; j++)
+            maze[i][j] = 0;
 
-	for(int i = 0; i < height; i++) // Массив заполняется землей-ноликами
-		for(int j = 0; j < width; j++)
-			maze[i][j] = 0;
-
-	x = 3; y = 3; a = 0; // Точка приземления крота и счетчик
-	while(a < 1000000){ // Да, простите, костыль, иначе есть как, но лень
-		maze[y][x] = 1; a++;
-		while(1){ // Бесконечный цикл, который прерывается только тупиком
-			c = rand()%4; // Напоминаю, что крот прорывает
-			switch(c){  // по две клетки в одном направлении за прыжок
-				case 0: if(y != 1)
-					if(maze[y-2][x] == 0){ // Вверх
+    x = 3; y = 3; a = 0; // Точка приземления крота и счетчик
+    while(a < 1000000){ // Да, простите, костыль, иначе есть как, но лень
+        maze[y][x] = 1; a++;
+        while(1){ // Бесконечный цикл, который прерывается только тупиком
+            c = rand()%4; // Напоминаю, что крот прорывает
+            switch(c){  // по две клетки в одном направлении за прыжок
+                case 0: if(y != 1)
+                    if(maze[y-2][x] == 0){ // Вверх
                                         maze[y-1][x] = 1;
-					maze[y-2][x] = 1;
-					y-=2;
-				}
-				case 1: if(y != height-2)
-					if(maze[y+2][x] == 0){ // Вниз
-					maze[y+1][x] = 1;
-					maze[y+2][x] = 1;
-					y+=2;
-				}
-				case 2: if(x != 1)
-					if(maze[y][x-2] == 0){ // Налево
-					maze[y][x-1] = 1;
-					maze[y][x-2] = 1;
-					x-=2;
-				}
-				case 3: if(x != width-2)
-					if(maze[y][x+2] == 0){ // Направо
-					maze[y][x+1] = 1;
-					maze[y][x+2] = 1;
-					x+=2;
-				}
-			}
-			if(deadend(x,y,maze,height,width))
-			   break;
-		}
+                    maze[y-2][x] = 1;
+                    y-=2;
+                }break;
+                case 1: if(y != height-2)
+                    if(maze[y+2][x] == 0){ // Вниз
+                    maze[y+1][x] = 1;
+                    maze[y+2][x] = 1;
+                    y+=2;
+                }
+                break;
+                case 2: if(x != 1)
+                    if(maze[y][x-2] == 0){ // Налево
+                    maze[y][x-1] = 1;
+                    maze[y][x-2] = 1;
+                    x-=2;
+                }
+                break;
+                case 3: if(x != width-2)
+                    if(maze[y][x+2] == 0){ // Направо
+                    maze[y][x+1] = 1;
+                    maze[y][x+2] = 1;
+                    x+=2;
+                }
+                break;
+            }
+            if(deadend(x,y,maze,height,width))
+               break;
+        }
 
-		if(deadend(x,y,maze,height,width)) // Вытаскиваем крота из тупика
-			do{
-				x = 2*(rand()%((width-1)/2))+1;
-				y = 2*(rand()%((height-1)/2))+1;
-			}
-			while(maze[y][x] != 1);
-	}
+        if(deadend(x,y,maze,height,width)) // Вытаскиваем крота из тупика
+            do{
+                x = 2*(rand()%((width-1)/2))+1;
+                y = 2*(rand()%((height-1)/2))+1;
+            }
+            while(maze[y][x] != 1);
+    }
 }
